877234df7edf55384e4c41f2ed46414f9a6ce51c
Ruan Ferreira <droderuan@hotmail.com>
2020-10-12 13:50:22 -0300

    Refactor function to create integers numbers for ID's

--- src/utils/GenerateID.ts
+++ src/utils/GenerateID.ts
@@ -1,3 +1,5 @@
 export default function GenerateID(): string {
-  return Math.random().toString(8);
+  const id = Math.random();
+  const parsedId = Math.floor(id * 100) + Date.now();
+  return parsedId.toString(10);
 }
