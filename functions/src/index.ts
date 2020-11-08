8f6e7f0f46e43b0c6a5dd75acaef190e55f70cb8
Ruan Ferreira <droderuan@hotmail.com>
2020-11-07 23:47:16 -0300

    Add firebase functions

---
+++ functions/src/index.ts
@@ -0,0 +1,40 @@
+import * as functions from 'firebase-functions';
+import { initializeApp } from 'firebase-admin';
+import path from 'path';
+import fs from 'fs';
+import handlebars from 'handlebars';
+import pdf from 'html-pdf';
+
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
+
+initializeApp({ projectId: 'app-orcamento-marmoraria' });
+
+export const templateTestSouthAmerica = functions
+  .region('southamerica-east1')
+  .https.onRequest(async (request, response) => {
+    const templatePath = path.resolve(
+      __dirname,
+      '..',
+      'templates',
+      'template.hbs',
+    );
+    const templateFile = await fs.promises.readFile(templatePath, {
+      encoding: 'utf-8',
+    });
+
+    const template = handlebars.compile(templateFile);
+    const parseTemplate = template({ name: 'ruan', legal: 'deudedeudh' });
+
+    pdf
+      .create(parseTemplate, {
+        type: 'pdf',
+        format: 'A4',
+        orientation: 'portrait',
+      })
+      .toBuffer((err, buffer) => {
+        if (err) return response.status(500).json(err);
+
+        return response.end(buffer);
+      });
+  });
